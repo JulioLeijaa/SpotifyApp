bb16689b93280a4839694e8f5e37eb680ac8c5c8
Castorena43 <hector.castorena.castillo@gmail.com>
2021-08-19 00:53:51 -0500

    [ADD] Components navigation and playlist

---
+++ resources/js/Pages/Components/sidebar/Navigation.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <div >
+            <img src="assets/spotify.png" width="175" height="40" alt="" srcset="">
+        </div>
+        <div class="flex flex-row place-items-center my-2">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mx-2" viewBox="0 0 20 20" fill="white">
+            <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+            </svg>
+            <span class="text-md text-white ml-2">Inicio</span>
+        </div>
+        <div class="flex flex-row place-items-center my-2">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mx-2" fill="none" viewBox="0 0 24 24" stroke="white">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+            </svg>
+            <span class="text-md text-white ml-2">Buscar</span>
+        </div>
+        <div class="flex flex-row place-items-center my-2">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mx-2" fill="none" viewBox="0 0 24 24" stroke="white">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
+            </svg>
+            <span class="text-md text-white ml-2">Tu biblioteca</span>
+        </div>
+        <div class="flex flex-row place-items-center mt-5">
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="white" class="bi bi-plus-square-fill mx-3" viewBox="0 0 16 16">
+            <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0z"/>
+            </svg>
+            <span class="text-md text-white ml-2">Crear Lista</span>
+        </div>
+        <div class="flex flex-row place-items-center my-2">
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="white" class="bi bi-heart-fill mx-3" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+            </svg>
+            <span class="text-md text-white ml-2">Canciones que te gustan</span>
+        </div>
+        
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Navigation'
+}
+</script>
+<style scoped>
+@import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css");
+</style>
---
+++ resources/js/Pages/Components/sidebar/Playlists.vue
@@ -0,0 +1,121 @@
+<template>
+    <div class="content h-48">
+        <ul>
+            <li v-for="playlist of playlists" :key="playlist.id" class="text-white text-opacity-75 m-2 text-sm">
+                {{playlist.name}}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Playlists',
+    data() {
+        return {
+            playlists:[
+                {
+                    'name':'Playlist1'
+                },
+                {
+                    'name':'Playlist1'
+                },
+                {
+                    'name':'Playlist1'
+                },
+                {
+                    'name':'Playlist1'
+                },
+                {
+                    'name':'Playlist1'
+                },
+                {
+                    'name':'Playlist1'
+                },
+                {
+                    'name':'Playlist1'
+                },
+                {
+                    'name':'Playlist1'
+                },
+                {
+                    'name':'Playlist1'
+                },
+                {
+                    'name':'Playlist1'
+                },
+                {
+                    'name':'Playlist1'
+                },
+                {
+                    'name':'Playlist1'
+                },
+                {
+                    'name':'Playlist1'
+                },
+                {
+                    'name':'Playlist1'
+                },
+                {
+                    'name':'Playlist1'
+                },
+                {
+                    'name':'Playlist1'
+                },
+                {
+                    'name':'Playlist1'
+                },
+                {
+                    'name':'Playlist1'
+                },
+                {
+                    'name':'Playlist1'
+                },
+                {
+                    'name':'Playlist1'
+                },
+                {
+                    'name':'Playlist1'
+                },
+                {
+                    'name':'Playlist1'
+                },
+                {
+                    'name':'Playlist1'
+                },
+                {
+                    'name':'Playlist1'
+                },
+                {
+                    'name':'Playlist1'
+                }
+            ]
+        }
+    },
+}
+</script>
+
+<style scoped>
+.content {
+    overflow-y: auto;
+}
+
+.content::-webkit-scrollbar {
+    width: 10px;
+    height: 6px;
+  }
+  .content::-webkit-scrollbar-track {
+    border-radius: 10px;
+    background: rgba(136, 136, 136, 0.1);
+  }
+  .content::-webkit-scrollbar-thumb{
+    border-radius: 10px;
+    background: rgba(0,0,0,0.2);
+  }
+  .content::-webkit-scrollbar-thumb:hover{
+  	background: rgba(0,0,0,0.4);
+  }
+  .content::-webkit-scrollbar-thumb:active{
+  	background: rgba(0,0,0,.9);
+  }
+</style>
